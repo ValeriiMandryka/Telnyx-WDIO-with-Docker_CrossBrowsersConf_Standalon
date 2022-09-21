1c517fd62cc4d019d473104dbd3f968435ec21b9
Valerii <mandryka.valerii@gmail.com>
2022-09-21 18:26:48 +0300

    modified:   conf/docker.conf.js

--- conf/docker.conf.js
+++ conf/docker.conf.js
@@ -8,18 +8,18 @@ capabilities: [
  {
    maxInstances: 5,
    browserName: "firefox",
-   'moz:firefoxOptions': 
-            {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
+  // 'moz:firefoxOptions': 
+  //          {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
  },
  {
    maxInstances: 5,
    browserName: "chrome",
-   'goog:chromeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
+ //  'goog:chromeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
  },
  {
     maxInstances: 5,
     browserName: 'MicrosoftEdge',
-    'ms:edgeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
+  //  'ms:edgeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
     acceptInsecureCerts: true
     },
 ],
