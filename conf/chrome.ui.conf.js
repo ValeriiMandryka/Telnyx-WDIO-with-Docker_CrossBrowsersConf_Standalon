791344881c1662acc46a202051e526b26cf3ea89
Valerii <mandryka.valerii@gmail.com>
2022-09-21 17:14:44 +0300

    modified:   conf/chrome.ui.conf.js

--- conf/chrome.ui.conf.js
+++ conf/chrome.ui.conf.js
@@ -4,32 +4,20 @@ const path = require("path");
 const chromeConfig = {
 ...config,
 
+
 services: [
-    ['selenium-standalone', {
-        //logPath: './temp',
-        // args: {
-        //     version: "3.141.59",
-        //    // seleniumArgs: ['-host', '127.0.0.1','-port', '5555']
-        // },
-    }]
+    ['selenium-standalone']
 ],
+
 capabilities: [{
-    browserName: 'chrome',
-   // port: 5555
+maxInstances: 1,
+browserName: "chrome",
+'goog:chromeOptions': {
+
+args: [ '--start-maximized', '--no-sandbox', '--disable -gpu',  '--allow-insecure-localhost']
+
+},
 }],
-// services: [
-//     ['selenium-standalone']
-// ],
-
-// capabilities: [{
-// maxInstances: 1,
-// browserName: "chrome",
-// 'goog:chromeOptions': {
-
-// args: [ '--start-maximized', '--no-sandbox', '--disable -gpu',  '--allow-insecure-localhost']
-
-// },
-// }],
 logLevel: 'warn',
 
 
