1689e6cfd6d397d34b86d0c81f358c9806dd81d9
Valerii <mandryka.valerii@gmail.com>
2022-09-21 16:52:27 +0300

    modified:   conf/chrome.ui.conf.js

--- conf/chrome.ui.conf.js
+++ conf/chrome.ui.conf.js
@@ -3,7 +3,9 @@ const path = require("path");
 
 const chromeConfig = {
 ...config,
-services: ["selenium-standalone", { drivers: {  chrome: true, chromiumedge: 'latest' } }],
+services: [
+    ['selenium-standalone', { drivers: { firefox: '0.29.1', chrome: true, chromiumedge: 'latest' } }]
+],
 capabilities: [{
 maxInstances: 1,
 browserName: "chrome",
