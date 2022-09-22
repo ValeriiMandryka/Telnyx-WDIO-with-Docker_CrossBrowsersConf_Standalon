9ce1db9de44eefb0a0ffb6b161d25bdc27e94ced
Valerii <mandryka.valerii@gmail.com>
2022-09-22 13:21:25 +0300

    modified:   .github/workflows/main.yml     	modified:   conf/firefox.headless.conf.js

--- conf/firefox.headless.conf.js
+++ conf/firefox.headless.conf.js
@@ -14,6 +14,11 @@ args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--w
 },
 }],
 logLevel: 'warn',
+reporters: [['allure', {
+    outputDir: 'allure-results',
+    disableWebdriverStepsReporting: true,
+    disableWebdriverScreenshotsReporting: true,
+}]],
 
 
 path: "/wd/hub",
