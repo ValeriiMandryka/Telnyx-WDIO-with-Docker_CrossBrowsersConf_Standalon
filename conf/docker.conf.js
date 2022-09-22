640a98f3d47986652adc1f285e01352363c2c340
Valerii <mandryka.valerii@gmail.com>
2022-09-22 12:29:16 +0300

    modified:   conf/docker.conf.js

--- conf/docker.conf.js
+++ conf/docker.conf.js
@@ -11,11 +11,11 @@ capabilities: [
    'moz:firefoxOptions': 
             {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
  },
- {
-   maxInstances: 5,
-   browserName: "chrome",
-  'goog:chromeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
- },
+//  {
+//    maxInstances: 5,
+//    browserName: "chrome",
+//   'goog:chromeOptions': {args: ['--headless', '--start-maximized', '--no-sandbox', '--disable -gpu', '--window-size=1920,780', '--allow-insecure-localhost']},
+//  },
 //  {
 //     maxInstances: 5,
 //     browserName: 'MicrosoftEdge',
